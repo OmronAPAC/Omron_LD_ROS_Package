844c674971e44158d635553cef7dff720a704b3d
Matthew Tan <tthmatt@gmail.com>
2019-11-07 14:10:46 +0800

    update

---
+++ src/om_aiv_util/scripts/analogInputQueryRaw_client.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import sys
+import rospy
+from om_aiv_util.srv import *
+
+def analogInputList_client(x):
+    rospy.wait_for_service('analogInputList')
+    try:
+        add_two_ints = rospy.ServiceProxy('analogInputList', Service)
+        resp1 = add_two_ints(x)
+        return resp1.device
+    except rospy.ServiceException, e:
+        print "Service call failed: %s"%e
+
+def usage():
+    return "%s [device]"%sys.argv[0]
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        x = str(sys.argv[1])
+    else:
+        print usage()
+        sys.exit(1)
+    print "running command"
+    # print "Requesting", x
+    analogInputList_client(x)
