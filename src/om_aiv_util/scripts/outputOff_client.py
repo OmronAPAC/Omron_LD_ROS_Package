dfb366751e867933cfff0f4d3e6c1d62f399e06f
Matthew Tan <tthmatt@gmail.com>
2019-11-08 15:54:04 +0800

    Create outputOff_client.py

---
+++ src/om_aiv_util/scripts/outputOff_client.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import sys
+import rospy
+from om_aiv_util.srv import *
+def outputOff_client(a, b):
+    rospy.wait_for_service('outputOff')
+    try:
+        add_two_ints = rospy.ServiceProxy('outputOff', Service)
+        resp1 = add_two_ints(a, b)
+        return resp1.device
+    except rospy.ServiceException, error:
+        print "Service call failed: %s"%error
+
+def usage():
+    return "%s <name>"%sys.argv[0]
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        a = str(sys.argv[1])
+    else:
+        print usage()
+        sys.exit(1)
+    print "running command"
+    # print "Requesting", x
+    print outputOff_client(a, b)
