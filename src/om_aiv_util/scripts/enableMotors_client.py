6d12a3b04417825559f7367f7f8fad6cb47e1441
Matthew Tan <tthmatt@gmail.com>
2019-11-08 10:12:44 +0800

    added enableMotors

---
+++ src/om_aiv_util/scripts/enableMotors_client.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+import sys
+import rospy
+from om_aiv_util.srv import *
+def analogInputQueryRaw_client(x):
+    rospy.wait_for_service('enableMotors')
+    try:
+        add_two_ints = rospy.ServiceProxy('enableMotors', Service)
+        resp1 = add_two_ints(x)
+        return resp1.device
+    except rospy.ServiceException, e:
+        print "Service call failed: %s"%e
+
+def usage():
+    return "%s [fault]"%sys.argv[0]
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        x = ""
+    else:
+        print usage()
+        sys.exit(1)
+    print "running command"
+    # print "Requesting", x
+    print analogInputQueryRaw_client(x)
