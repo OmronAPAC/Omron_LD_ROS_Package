efee0df9c4c303be1a692690d52ee37189120714
Matthew Tan <tthmatt@gmail.com>
2019-11-08 16:12:50 +0800

    added payloadQueryLocal

---
+++ src/om_aiv_util/scripts/payloadQueryLocal_client.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+import sys
+import rospy
+from om_aiv_util.srv import *
+def payloadQueryLocal_client(x):
+    rospy.wait_for_service('payloadQueryLocal')
+    try:
+        add_two_ints = rospy.ServiceProxy('payloadQueryLocal', Service)
+        resp1 = add_two_ints(x)
+        return resp1.device
+    except rospy.ServiceException, e:
+        print "Service call failed: %s"%e
+
+def usage():
+    return "%s [fault]"%sys.argv[0]
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        x = str(sys.argv[1])
+    if len(sys.argv) == 1:
+        x = ""
+    else:
+        print usage()
+        sys.exit(1)
+    print "running command"
+    # print "Requesting", x
+    print payloadQueryLocal_client(x)
---
+++ src/om_aiv_util/scripts/payloadQueryLocal_server.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+from socketconnection_class import ConnectSocket, connecttcp
+s = connecttcp.sock
+import socket
+import threading
+import time
+import re
+import sys
+from std_msgs.msg import String
+BUFFER_SIZE = 1024
+# ip_address = rospy.get_param("ip_address")
+# port = rospy.get_param("port")
+ip_address = "172.21.5.125"
+port = 7171
+connecttcp.connect(str(ip_address), port)
+
+from om_aiv_util.srv import Service,ServiceResponse
+import rospy
+
+def handle_payloadQueryLocal(req):
+    global fault
+    print "Returning", req.a
+    slot_num = req.a
+    payloadQueryLocal()
+    # return ServiceResponse(req.a)
+    return rcv
+
+def payloadQueryLocal_server():
+    rospy.init_node('payloadQueryLocal_server')
+    s = rospy.Service('payloadQueryLocal', Service, handle_payloadQueryLocal)
+    rospy.spin()
+
+def payloadQueryLocal():
+    global rcv
+    pub = rospy.Publisher('arcl_payloadQueryLocal', String, queue_size=10)
+    # rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    command = "payloadQueryLocal {}".format(slot_num)
+    print "Running command: ", command
+    command = command.encode('ascii')
+    s.send(command+b"\r\n")
+    try:
+        data = s.recv(BUFFER_SIZE)
+        rcv = data.encode('ascii', 'ignore')
+        while not rospy.is_shutdown():
+            #check for required data
+            if "EndPayloadQuery" in rcv:
+                print rcv
+                return rcv
+                break
+            if "CommandErrorDescription" in rcv:
+                print rcv
+                return rcv
+                break
+            else:
+                data = s.recv(BUFFER_SIZE)
+                rcv = rcv + data.encode('ascii', 'ignore')
+
+    except socket.error as e:
+        print("Connection  failed")
+        return e
+
+if __name__ == "__main__":
+    payloadQueryLocal_server()
