1448b0f5e610df3e88c671741f4e68d839554c57
Matthew Tan <tthmatt@gmail.com>
2019-11-08 11:18:33 +0800

    Update getConfigSectionList_client.py

--- src/om_aiv_util/scripts/getConfigSectionList_client.py
+++ src/om_aiv_util/scripts/getConfigSectionList_client.py
@@ -12,7 +12,7 @@ def analogInputQueryRaw_client(x):
         print "Service call failed: %s"%e
 
 def usage():
-    return "%s [fault]"%sys.argv[0]
+    return "%s"%sys.argv[0]
 
 if __name__ == "__main__":
     if len(sys.argv) == 1:
