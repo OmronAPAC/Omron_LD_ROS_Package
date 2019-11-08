aa69dc0519576e2f3d52ce25c4ca1dc8dbca589b
Matthew Tan <tthmatt@gmail.com>
2019-11-08 10:39:45 +0800

    added getConfigSectionInfo

---
+++ src/om_aiv_util/scripts/getConfigSectionInfo_client.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import sys
+import rospy
+from om_aiv_util.srv import *
+def getConfigSectionInfo_client(x):
+    rospy.wait_for_service('getConfigSectionInfo')
+    try:
+        add_two_ints = rospy.ServiceProxy('getConfigSectionInfo', Service)
+        resp1 = add_two_ints(x)
+        return resp1.device
+    except rospy.ServiceException, e:
+        print "Service call failed: %s"%e
+
+def usage():
+    return "%s <section>"%sys.argv[0]
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        x = str(sys.argv[1])
+    else:
+        print usage()
+        sys.exit(1)
+    print "running command"
+    # print "Requesting", x
+    getConfigSectionInfo_client(x)
+    print getConfigSectionInfo_client(x)
