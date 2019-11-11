10aa8ac657f332811d17d2e02752ec3020a3ceb9
Matthew Tan <tthmatt@gmail.com>
2019-11-11 08:57:28 +0800

    added payloadSet

---
+++ src/om_aiv_util/scripts/payloadSet_client.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import sys
+import rospy
+from om_aiv_util.srv import *
+def payloadSet_client(a, b):
+    rospy.wait_for_service('payloadSet')
+    try:
+        add_two_ints = rospy.ServiceProxy('payloadSet', Service2)
+        resp1 = add_two_ints(a, b)
+        return resp1.device
+    except rospy.ServiceException, error:
+        print "Service call failed: %s"%error
+
+def usage():
+    return "%s <slot_number> <slot_string>"%sys.argv[0]
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        a = str(sys.argv[1])
+        b = str(sys.argv[2])
+    else:
+        print usage()
+        sys.exit(1)
+    print "running command"
+    # print "Requesting", x
+    print payloadSet_client(a, b)
