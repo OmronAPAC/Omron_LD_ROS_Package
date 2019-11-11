bc16abc71d4a03ac1c0cf83697f8f002a0e43b74
Matthew Tan <tthmatt@gmail.com>
2019-11-11 15:30:35 +0800

    added updateInfo

---
+++ src/om_aiv_util/scripts/updateInfo_client.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+import sys
+import rospy
+from om_aiv_util.srv import *
+def updateInfo_client(a, b):
+    rospy.wait_for_service('updateInfo')
+    try:
+        add_two_ints = rospy.ServiceProxy('updateInfo', Service2)
+        resp1 = add_two_ints(a, b)
+        return resp1.device
+    except rospy.ServiceException, error:
+        print "Service call failed: %s"%error
+
+def usage():
+    return "%s <task> <argument>"%sys.argv[0]
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        a = str(sys.argv[1])
+        b = str(sys.argv[2])
+    else:
+        print usage()
+        sys.exit(1)
+    print "running command"
+    # print "Requesting", x
+    print updateInfo_client(a, b)
