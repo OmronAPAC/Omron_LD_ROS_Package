3cc47947a87238e36e1f9446920f06d911915d87
Matthew Tan <tthmatt@gmail.com>
2019-11-11 13:27:35 +0800

    added QueueDropOff

---
+++ src/om_aiv_util/scripts/queueDropoff_client.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+import sys
+import rospy
+from om_aiv_util.srv import *
+def analogInputQueryRaw_client(a, b , c):
+    rospy.wait_for_service('createInfo')
+    try:
+        add_two_ints = rospy.ServiceProxy('createInfo', Service3)
+        resp1 = add_two_ints(a, b, c)
+        return resp1.device
+    except rospy.ServiceException, error:
+        print "Service call failed: %s"%error
+
+def usage():
+if __name__ == "__main__":
+    if len(sys.argv) == 4:
+        a = str(sys.argv[1])
+        b = str(sys.argv[2])
+        c = str(sys.argv[3])
+    elif len(sys.argv) == 2:
+        a = str(sys.argv[1])
+        b = ""
+        c = ""
+    else:
+        print usage()
+        sys.exit(1)
+    print "running command"
+    # print "Requesting", x
+    print analogInputQueryRaw_client(a, b , c)
---
+++ src/om_aiv_util/scripts/queueDropoff_server.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+from socketconnection_class import ConnectSocket, connecttcp
+s = connecttcp.sock
+import socket
+import threading
+import time
+import re
+import sys
+from std_msgs.msg import String
+BUFFER_SIZE = 1024
+# ip_address = rospy.get_param("ip_address")
+# port = rospy.get_param("port")
+ip_address = "172.21.5.125"
+port = 7171
+connecttcp.connect(str(ip_address), port)
+
+from om_aiv_util.srv import Service3,Service3Response
+import rospy
+
+def handle_createInfo(req):
+    global a, b, c
+    a = req.a
+    b = req.b
+    c = req.c
+    createInfo()
+    # return Service5Response(req.a + req.b + req.c + req.d + req.e)
+    return rcv
+
+def createInfo_server():
+    rospy.init_node('createInfo_server')
+    s = rospy.Service('createInfo', Service3, handle_createInfo)
+    rospy.spin()
+
+def createInfo():
+    global rcv
+    pub = rospy.Publisher('arcl_createInfo', String, queue_size=10)
+    # rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    command = "queueDropoff {}".format(a + " " + b + " " + c)
+    command = command.encode('ascii')
+    print "Running command: ", command
+    s.send(command+b"\r\n")
+    try:
+        data = s.recv(BUFFER_SIZE)
+        rcv = data.encode('ascii', 'ignore')
+        while not rospy.is_shutdown():
+            #check for required data
+            if "sucessfully " in rcv:
+                print rcv
+                return rcv
+                break
+            if "CommandErrorDescription" in rcv:
+                print rcv
+                return rcv
+                break
+            else:
+                data = s.recv(BUFFER_SIZE)
+                rcv = rcv + data.encode('ascii', 'ignore')
+
+    except socket.error as errormsg:
+        print("Connection  failed")
+        return errormsg
+
+if __name__ == "__main__":
+    createInfo_server()
