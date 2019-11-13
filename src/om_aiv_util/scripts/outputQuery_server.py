fbab1d2164b0c19217d0250830beea871c72ad56
Matthew Tan <tthmatt@gmail.com>
2019-11-13 15:59:24 +0800

    added outputQuery

---
+++ src/om_aiv_util/scripts/outputQuery_server.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+from socketconnection_class import ConnectSocket, connecttcp
+s = connecttcp.sock
+import socket
+import threading
+import time
+import re
+import sys
+from std_msgs.msg import String
+BUFFER_SIZE = 1024
+# ip_address = rospy.get_param("ip_address")
+# port = rospy.get_param("port")
+ip_address = "168.3.201.123"
+port = 7171
+connecttcp.connect(str(ip_address), port)
+
+from om_aiv_util.srv import Service,ServiceResponse
+import rospy
+
+def handle_outputQuery(req):
+    global text
+    text = req.a
+    outputQuery()
+    # return ServiceResponse(req.a)
+    return rcv
+
+def outputQuery_server():
+    rospy.init_node('outputQuery_server')
+    s = rospy.Service('outputQuery', Service, handle_outputQuery)
+    rospy.spin()
+
+def outputQuery():
+    global rcv
+    pub = rospy.Publisher('arcl_outputQuery', String, queue_size=10)
+    # rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    command = "outputQuery {}".format(text)
+    command = command.encode('ascii')
+    print "Running command: ", command
+    s.send(command+b"\r\n")
+    try:
+        data = s.recv(BUFFER_SIZE)
+        rcv = data.encode('ascii', 'ignore')
+        while not rospy.is_shutdown():
+            #check for required data
+            if "Output:" in rcv:
+                print rcv
+                return rcv
+                break
+            if "CommandErrorDescription" in rcv:
+                print rcv
+                return rcv
+                break
+            else:
+                data = s.recv(BUFFER_SIZE)
+                rcv = rcv + data.encode('ascii', 'ignore')
+
+    except socket.error as e:
+        print("Connection  failed")
+        return e
+
+if __name__ == "__main__":
+    outputQuery_server()
