5598d55f3bfd983079d65e210d991824702a66df
Matthew Tan <tthmatt@gmail.com>
2019-11-21 11:04:02 +0800

    testing 2robots service

--- src/om_aiv_util/scripts/enableMotors_client.py
+++ src/om_aiv_util/scripts/enableMotors_client.py
@@ -2,10 +2,16 @@
 import sys
 import rospy
 from om_aiv_util.srv import *
-def enableMotors_client(x):
-    rospy.wait_for_service('enableMotors')
+def enableMotors_client(x,y):
+    service_name = "/{}".format(y + "/enableMotors")
+    print service_name
+    print y
+    # rospy.wait_for_service('enableMotors')
+    rospy.wait_for_service(service_name)
+    # rospy.wait_for_service('/robot1/enableMotors')
     try:
-        service = rospy.ServiceProxy('enableMotors', Service)
+        # service = rospy.ServiceProxy('robot1/enableMotors', Service)
+        service = rospy.ServiceProxy(service_name, Service)
         resp1 = service(x)
         return resp1.device
     except rospy.ServiceException, e:
@@ -15,10 +21,12 @@ def usage():
     return "%s"%sys.argv[0]
 
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
+    if len(sys.argv) == 2:
         x = ""
+        y = str(sys.argv[1])
+
     else:
         print usage()
         sys.exit(1)
     print "running command"
-    print enableMotors_client(x)
+    print enableMotors_client(x, y)
