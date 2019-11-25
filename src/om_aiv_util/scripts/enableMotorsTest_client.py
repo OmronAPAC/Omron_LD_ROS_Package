f602f4b6e7a93116c956e92571c8e3a1f258762b
Matthew Tan <tthmatt@gmail.com>
2019-11-25 15:38:55 +0800

    update

--- src/om_aiv_util/scripts/enableMotorsTest_client.py
+++ src/om_aiv_util/scripts/enableMotorsTest_client.py
@@ -3,9 +3,14 @@ import sys
 import rospy
 from om_aiv_util.srv import *
 def enableMotors_client(x):
-    rospy.wait_for_service('enableMotors')
+    service_name = "/{}".format(x + "/enableMotors")
+    print service_name
+    # rospy.wait_for_service('enableMotors')
+    rospy.wait_for_service(service_name, timeout=30)
+    # rospy.wait_for_service('/robot1/enableMotors')
     try:
-        service = rospy.ServiceProxy('enableMotors', Service)
+        # service = rospy.ServiceProxy('robot1/enableMotors', Service)
+        service = rospy.ServiceProxy(service_name, Service)
         resp1 = service(x)
         return resp1.device
     except rospy.ServiceException, e:
@@ -15,8 +20,8 @@ def usage():
     return "%s"%sys.argv[0]
 
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
-        x = ""
+    if len(sys.argv) == 2:
+        x = str(sys.argv[1])
 
     else:
         print usage()
