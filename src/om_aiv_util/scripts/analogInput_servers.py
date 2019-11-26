c4e63b908c2df9095c32ed9226d87481deb460ee
Matthew Tan <tthmatt@gmail.com>
2019-11-26 13:43:28 +0800

    Revert "ana"
    This reverts commit cbf85ebe73ae5bd72dec700b6d88aba457f291a3.

--- src/om_aiv_util/scripts/analogInput_servers.py
+++ src/om_aiv_util/scripts/analogInput_servers.py
@@ -1,137 +1,28 @@
 #!/usr/bin/env python
-from om_aiv_util.srv import Service,ServiceResponse
+
+from om_aiv_util.srv import AddTwoInts,AddTwoIntsResponse
 import rospy
-from socketconnection_class import ConnectSocket, connecttcp
-s = connecttcp.sock
-import socket
-import threading
-import time
-import re
-import sys
-import rospy
-from std_msgs.msg import String
-BUFFER_SIZE = 1024
-# ip_address = rospy.get_param("ip_address")
-# port = rospy.get_param("port")
-ip_address = "172.21.5.123"
-port = 7171
-connecttcp.connect(str(ip_address), port)
 
-def handle_analogInputList(req):
-    analogInputList()
-    return rcv
-def handle_analogInputQueryRaw(req):
-    global text
-    text = req.a
-    analogInputQueryRaw()
-    return rcv
-def handle_analogInputQueryVoltage(req):
-    global text
-    text = req.a
-    analogInputQueryVoltage()
-    return rcv
+def handle_add_two_ints(req):
+    print "Returning [%s + %s = %s]"%(req.a, req.b, (req.a + req.b))
+    return AddTwoIntsResponse(req.a + req.b)
+def handle_minus_two_ints(req):
+    print "Returning [%s - %s = %s]"%(req.a, req.b, (req.a - req.b))
+    return AddTwoIntsResponse(req.a - req.b)
 
-def analogInputList_servers(op):
-    # print op
-    if op == "List":
-        print "running List"
-        s1 = rospy.Service('analogInputList', Service, handle_analogInputList)
-    elif op == "QueryRaw":
-        print "running QueryRaw"
-        s2 = rospy.Service('analogInputQueryRaw', Service, handle_analogInputQueryRaw)
-    elif op == "QueryVoltage":
-        print "running QueryVoltage"
-        s3 = rospy.Service('analogInputQueryVoltage', Service, handle_analogInputQueryVoltage)
-
-def analogInputList():
-    global rcv
-    pub = rospy.Publisher('arcl_analogInputList', String, queue_size=10)
-    rate = rospy.Rate(10) # 10hz
-
-    command = "analogInputList"
-    command = command.encode('ascii')
-    s.send(command+b"\r\n")
-    try:
-        data = s.recv(BUFFER_SIZE)
-        rcv = data.encode('ascii', 'ignore')
-        while not rospy.is_shutdown():
-            #check for required data
-            if "End of AnalogInputList" in rcv:
-
-                print rcv
-                rcv = str(rcv.splitlines())
-                break
-
-            else:
-                data = s.recv(BUFFER_SIZE)
-                rcv = rcv + data.encode('ascii', 'ignore')
-
-    except socket.error as e:
-        print("Connection  failed")
-        return e
-
-def analogInputQueryRaw():
-    global rcv
-    pub = rospy.Publisher('arcl_analogInputQueryRaw', String, queue_size=10)
-    rate = rospy.Rate(10) # 10hz
-    command = "analogInputQueryRaw {}".format(text)
-    command = command.encode('ascii')
-    print "Running command: ", command
-    s.send(command+b"\r\n")
-    try:
-        data = s.recv(BUFFER_SIZE)
-        rcv = data.encode('ascii', 'ignore')
-        while not rospy.is_shutdown():
-            #check for required data
-            if "AnalogInputRaw:" in rcv:
-                print rcv
-                return rcv
-                break
-            if "CommandErrorDescription" in rcv:
-                print rcv
-                return rcv
-                break
-            else:
-                data = s.recv(BUFFER_SIZE)
-                rcv = rcv + data.encode('ascii', 'ignore')
-
-    except socket.error as e:
-        print("Connection  failed")
-        return e
-
-def analogInputQueryVoltage():
-    global rcv
-    pub = rospy.Publisher('arcl_analogInputQueryVoltage', String, queue_size=10)
-    rate = rospy.Rate(10) # 10hz
-    command = "analogInputQueryVoltage {}".format(text)
-    command = command.encode('ascii')
-    print "Running command: ", command
-    s.send(command+b"\r\n")
-    try:
-        data = s.recv(BUFFER_SIZE)
-        rcv = data.encode('ascii', 'ignore')
-        while not rospy.is_shutdown():
-            #check for required data
-            if "AnalogInputVoltage:" in rcv:
-                print rcv
-                return rcv
-                break
-            if "CommandErrorDescription" in rcv:
-                print rcv
-                return rcv
-                break
-            else:
-                data = s.recv(BUFFER_SIZE)
-                rcv = rcv + data.encode('ascii', 'ignore')
-
-    except socket.error as e:
-        print("Connection  failed")
-        return e
+def operation_two_ints_server(op):
+    # rospy.init_node('add_two_ints_server')
+    print op
+    if op == "plus":
+        s = rospy.Service('add_two_ints', AddTwoInts, handle_add_two_ints)
+        print "Ready to add two ints."
+    elif op == "minus":
+        s2 = rospy.Service('minus_two_ints', AddTwoInts, handle_minus_two_ints)
+        print "Ready to subtract two ints."
 
 
 if __name__ == "__main__":
-    rospy.init_node('analogInput_servers')
-    analogInputList_servers("List")
-    analogInputList_servers("QueryRaw")
-    analogInputList_servers("QueryVoltage")
+    rospy.init_node('operations_server')
+    operation_two_ints_server("plus")
+    operation_two_ints_server("minus")
     rospy.spin()
