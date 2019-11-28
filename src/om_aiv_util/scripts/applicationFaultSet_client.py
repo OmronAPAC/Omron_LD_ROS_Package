ae19a560895c5f0ce1f86222c77a6dcf08d1db72
Matthew Tan <tthmatt@gmail.com>
2019-11-28 09:39:52 +0800

    Update applicationFaultSet_client.py

--- src/om_aiv_util/scripts/applicationFaultSet_client.py
+++ src/om_aiv_util/scripts/applicationFaultSet_client.py
@@ -2,11 +2,11 @@
 import sys
 import rospy
 from om_aiv_util.srv import *
-def analogInputQueryRaw_client(a, b , c, d, e):
+def applicationFaultSet_client(array):
     rospy.wait_for_service('applicationFaultSet')
     try:
-        service = rospy.ServiceProxy('applicationFaultSet', Service5)
-        resp1 = service(a, b, c, d, e)
+        service = rospy.ServiceProxy('applicationFaultSet', OmAivService)
+        resp1 = service(array)
         return resp1.device
     except rospy.ServiceException, error:
         print "Service call failed: %s"%error
@@ -16,13 +16,14 @@ def usage():
 
 if __name__ == "__main__":
     if len(sys.argv) == 6:
-        a = str(sys.argv[1])
-        b = str(sys.argv[2])
-        c = str(sys.argv[3])
-        d = str(sys.argv[4])
-        e = str(sys.argv[5])
+        name = str(sys.argv[1])
+        short_description = str(sys.argv[2])
+        long_description = str(sys.argv[3])
+        bool_driving = str(sys.argv[4])
+        bool_critical = str(sys.argv[5])
+        array = [name, short_description, long_description, bool_driving, bool_critical]
     else:
         print usage()
         sys.exit(1)
     print "running command"
-    print analogInputQueryRaw_client(a, b , c, d, e)
+    print applicationFaultSet_client(array)
