63b48e38cfb94f4280da7ef2b1a033cc6f9bee90
Matthew Tan <tthmatt@gmail.com>
2019-12-09 15:57:49 +0800

    Update multiple_robots_example_client.py

--- src/om_aiv_util/scripts/multiple_robots_example_client.py
+++ src/om_aiv_util/scripts/multiple_robots_example_client.py
@@ -2,16 +2,13 @@
 import sys
 import rospy
 from om_aiv_util.srv import *
-def enableMotors_client(x):
-    service_name = "/{}".format(x + "/enableMotors")
+def enableMotors_client(robot_name):
+    service_name = "/{}".format(robot_name + "/enableMotors")
     print service_name
-    # rospy.wait_for_service('enableMotors')
     rospy.wait_for_service(service_name, timeout=30)
-    # rospy.wait_for_service('/robot1/enableMotors')
     try:
-        # service = rospy.ServiceProxy('robot1/enableMotors', Service)
         service = rospy.ServiceProxy(service_name, Service)
-        resp1 = service(x)
+        resp1 = service(robot_name)
         return resp1.device
     except rospy.ServiceException, e:
         print "Service call failed: %s"%e
@@ -21,10 +18,10 @@ def usage():
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
-        x = str(sys.argv[1])
+        robot_name = str(sys.argv[1])
 
     else:
         print usage()
         sys.exit(1)
     print "running command"
-    print enableMotors_client(x)
+    print enableMotors_client(robot_name)
