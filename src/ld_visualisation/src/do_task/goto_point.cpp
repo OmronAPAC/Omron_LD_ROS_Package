098ec2226234750bcf42405df801ca757b41208a
lekoook <kokteng1313@gmail.com>
2020-01-22 20:50:18 +0800

    Modify gotoPoint node to use 2D Nav Goal tool

--- src/ld_visualisation/src/do_task/goto_point.cpp
+++ src/ld_visualisation/src/do_task/goto_point.cpp
@@ -1,5 +1,5 @@
 #include <ros/ros.h>
-#include <geometry_msgs/PoseWithCovarianceStamped.h>
+#include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/Quaternion.h>
 #include <tf/transform_datatypes.h>
 #include <om_aiv_util/ArclApi.h>
@@ -10,8 +10,9 @@ ros::ServiceClient arcl_api_client;
 const std::string API_SRV_NAME = "arcl_api_service";
 const std::string GOTO_CMD = "doTask gotoPoint ";
 const std::string LINE_ID = "Doing task gotoPoint ";
+const std::string POINT_TOPIC = "/move_base_simple/goal";
 
-void point_cb(const geometry_msgs::PoseWithCovarianceStampedConstPtr& point_msg);
+void point_cb(const geometry_msgs::PoseStampedConstPtr& point_msg);
 
 int main(int argc, char** argv)
 {
@@ -19,7 +20,7 @@ int main(int argc, char** argv)
     ros::NodeHandle nh;
     ros::Rate rate(10);
     
-    ros::Subscriber point_sub = nh.subscribe<geometry_msgs::PoseWithCovarianceStamped>("/initialpose", 10, point_cb);
+    ros::Subscriber point_sub = nh.subscribe<geometry_msgs::PoseStamped>(POINT_TOPIC, 10, point_cb);
     arcl_api_client = nh.serviceClient<om_aiv_util::ArclApi>(API_SRV_NAME);
 
     ros::spin();
@@ -27,16 +28,21 @@ int main(int argc, char** argv)
     return 0;
 }
 
-void point_cb(const geometry_msgs::PoseWithCovarianceStampedConstPtr& point_msg)
+/**
+ * @brief Callback function when the point to go to is published on topic.
+ * 
+ * @param point_msg Message containing the x, y and theta of the goto point.
+ */
+void point_cb(const geometry_msgs::PoseStampedConstPtr& point_msg)
 {
     // Get the pose published.
     double dummy_roll;
     double dummy_pitch;
     double theta;
-    int x = point_msg->pose.pose.position.x * 1000;
-    int y = point_msg->pose.pose.position.y * 1000;
+    int x = point_msg->pose.position.x * 1000;
+    int y = point_msg->pose.position.y * 1000;
     tf::Quaternion tf_quat;
-    tf::quaternionMsgToTF(point_msg->pose.pose.orientation, tf_quat);
+    tf::quaternionMsgToTF(point_msg->pose.orientation, tf_quat);
     tf::Matrix3x3(tf_quat).getRPY(dummy_roll, dummy_pitch, theta);
     theta *= 57.29577951; // Convert to degree
     if (theta > 180) theta -= 360;
@@ -48,11 +54,8 @@ void point_cb(const geometry_msgs::PoseWithCovarianceStampedConstPtr& point_msg)
     req_msg.request.command += vals_str;
     req_msg.request.line_identifier += vals_str;
     // Send the request to ARCL api server.
-    ROS_INFO("cmd: %s", req_msg.request.command.c_str());
-    ROS_INFO("id: %s", req_msg.request.line_identifier.c_str());
     if (arcl_api_client.call(req_msg))
     {
-        ROS_INFO("resp: %s", req_msg.response.response.c_str());
         if (req_msg.response.response.find(LINE_ID) != std::string::npos)
         {
             ROS_INFO("Executing %s", req_msg.request.command.c_str());
@@ -61,11 +64,9 @@ void point_cb(const geometry_msgs::PoseWithCovarianceStampedConstPtr& point_msg)
         {
             ROS_ERROR("gotoPoint task failed, response: %s", req_msg.response.response.c_str());
         }
-        
     }
     else
     {
         ROS_ERROR("Failed to call %s service for gotoPoint task", API_SRV_NAME);
     }
-    
 }
